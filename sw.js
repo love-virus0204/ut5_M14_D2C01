f6d69573b11ca64d65c5af3319b6422e103bca99
愛情病毒 <nak.visu@gmail.com>
2025-10-14 15:05:51 +0800

    创建 sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+// sw.js
+const CACHE = 'app-v1';
+const ASSETS = [
+  './mus/Prontera.mp3'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(
+    caches.match(req).then(hit =>
+      hit || fetch(req).then(res => {
+        if (res.ok && req.url.startsWith(self.location.origin)) {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(req, copy));
+        }
+        return res;
+      })
+    )
+  );
+});
